6ad2a89e6c7fb49bd4b4f19b709acb6ce1b37c15
manjarb <manjarb4@gmail.com>
2016-02-03 15:55:10 +0800

    updatee

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def ful_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
