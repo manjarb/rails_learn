1871299057231602d248039285bbdf66effa60f3
manjarb <manjarb4@gmail.com>
2016-02-04 17:50:00 +0800

    updateeeeeeee

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,6 +6,22 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
+      if params[:session][:remember_me] == '1'
+        puts "########################################################################"
+        puts "########################################################################"
+        puts "rememmm"
+        puts "########################################################################"
+        puts "########################################################################"
+      else
+        puts "########################################################################"
+        puts "########################################################################"
+        puts "no rememmmm"
+        puts "########################################################################"
+        puts "########################################################################"
+      end
+
       redirect_to user
     else
       flash.now[:danger] = "Wrong email or pass"
@@ -14,7 +30,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
